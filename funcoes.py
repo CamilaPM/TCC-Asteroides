ee3dfc56ec75c51d6faea9484ee790cb4ed45e49
CamilaPM <50966338+CamilaPM@users.noreply.github.com>
2021-10-18 13:35:51 -0300

    Update funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -1,20 +1,17 @@
 import numpy as np
 
 def q (G): #onde G é o parâmetro de inclinação, às vezes adotado como 0.15
-  G = float(input("Digite o valor do parâmetro de inclinação: "))
 	q = 0.29 + 0.684 * G
 	return q
 
 ##############################################################
 
 def pv (A, q): #onde A é o albedo bolométrico de Bond e q é uma integral de fase
-	A = float(input("Digite o valor do albedo bolométrico: "))
 	pv = A/q
 	return pv
 
 ##############################################################
 
-def H (D, pv): #onde H é a magnitude absoluta, D é o diâmetro e pv é o albedo geométrico
-	D = float(input("Digite o valor do diâmetro em km: "))
+def H (D, pv): #onde H é a magnitude absoluta, D é o diâmetro, dado em km e pv é o albedo geométrico
 	H = (-5*math.log(D)) - ((5/2)*math.log(pv)) + 15.62
 	return H
